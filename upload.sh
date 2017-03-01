387b8e61de4ac02d4e6af36fa22378d4a796d844
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2017-03-01 22:51:22 +0000

    baconthulhu: add distrib script
    git-svn-id: https://rpg.hamsterrepublic.com/source/games/baconthulhu@8479 7d344553-34f0-0310-a9b1-970ce8f1c3a2

---
+++ upload.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+scp -p baconthulhu.zip james_paige@motherhamster.org:HamsterRepublic.com/dl/
+scp -p setup-baconthulhu.exe james_paige@motherhamster.org:HamsterRepublic.com/dl/baconthulhu.exe
+scp -p baconthulhu-mac.tar.gz james_paige@motherhamster.org:HamsterRepublic.com/dl/
